a5eae82a98f3248ccf7c6f382a16954df1855c06
Saucelo <jacobsaucelo32@gmail.com>
2024-05-10 16:53:14 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jacobsaucelo
+
+go 1.22.1
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+type FileType struct {
+	Name        string `json:Name`
+	UserName    string `json:UserName`
+	Status      int    `json:Status`
+	CreatedDate string `json:Created Date`
+	UpdatedDate string `json:Updated Date`
+}
+
+func main() {
+
+}
